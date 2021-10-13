499624a97b42fe591ded66a2b9e85f6ebab68fa4
aguschirico <aguschirico@gmail.com>
2021-10-13 12:59:31 +0200

    fix deployer.

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -6,9 +6,9 @@ var SafeMath = artifacts.require('SafeMath');
 module.exports = async function(deployer, network) {
    if (network == 'testnet') { //used for running truffle tests
         await deployer.deploy(BridgeMock);
-        const mockInstance = await BridgeMock.deployed(); // get the deployed instance of A
-
+        const mockInstance = await BridgeMock.deployed();
         let bridgeAddress = mockInstance.address;
+        
         await deployer.deploy(SafeMath);
         await deployer.link(SafeMath, LiquidityBridgeContract);
         await deployer.deploy(LiquidityBridgeContract, bridgeAddress, 1, 10, 1, 2300 * 65164000);
