0c4db2d9e70458c8917dc8ad3b6da98070f4af87
Guilherme Soares <guilherme.soares@iovlabs.org>
2023-03-20 15:52:26 +0200

    Comment upgrade script

--- migrations/3_upgrade_contracts.js
+++ migrations/3_upgrade_contracts.js
@@ -1,30 +1,30 @@
 const { upgradeProxy } = require("@openzeppelin/truffle-upgrades");
 
-const version = "V2";
+// const version = "V2";
 
-const SignatureValidator = artifacts.require("SignatureValidator");
-const LiquidityBridgeContract = artifacts.require(`LiquidityBridgeContract`);
-const LiquidityBridgeContractUpgrade = artifacts.require(
-  `LiquidityBridgeContract${version}`
-);
+// const SignatureValidator = artifacts.require("SignatureValidator");
+// const LiquidityBridgeContract = artifacts.require(`LiquidityBridgeContract`);
+// const LiquidityBridgeContractUpgrade = artifacts.require(
+//   `LiquidityBridgeContract${version}`
+// );
 
-const { deploy, read } = require("../config");
+// const { deploy, read } = require("../config");
 
-module.exports = async function (deployer, network) {
-  let config = read();
-  config = await deploy("LiquidityBridgeContract", network, async (state) => {
-    const signatureValidatorLib = await SignatureValidator.at(
-      config[network]["SignatureValidator"].address
-    );
-    await deployer.link(signatureValidatorLib, LiquidityBridgeContractUpgrade);
+// module.exports = async function (deployer, network) {
+//   let config = read();
+//   config = await deploy("LiquidityBridgeContract", network, async (state) => {
+//     const signatureValidatorLib = await SignatureValidator.at(
+//       config[network]["SignatureValidator"].address
+//     );
+//     await deployer.link(signatureValidatorLib, LiquidityBridgeContractUpgrade);
 
-    const existing = await LiquidityBridgeContract.deployed();
-    const response = await upgradeProxy(
-      existing.address,
-      LiquidityBridgeContractUpgrade,
-      { deployer, unsafeAllowLinkedLibraries: true }
-    );
-    console.log("Upgraded", response.address);
-    state.address = response.address;
-  });
-};
+//     const existing = await LiquidityBridgeContract.deployed();
+//     const response = await upgradeProxy(
+//       existing.address,
+//       LiquidityBridgeContractUpgrade,
+//       { deployer, unsafeAllowLinkedLibraries: true }
+//     );
+//     console.log("Upgraded", response.address);
+//     state.address = response.address;
+//   });
+// };
