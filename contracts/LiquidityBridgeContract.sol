65b1ce7557ffc6edeadca834c2465c6d9b1b4d1e
Luisfc68 <luisfc68@gmail.com>
2023-10-03 16:17:29 -0300

    VULN-183: validate initialization arguments

--- contracts/LiquidityBridgeContract.sol
+++ contracts/LiquidityBridgeContract.sol
@@ -156,6 +156,8 @@ contract LiquidityBridgeContract is Initializable, OwnableUpgradeable, Reentranc
         bool _mainnet
     ) external initializer {
         require(_rewardPercentage <= 100, "LBC004");
+        require(_minimumCollateral >= 0.6 ether, "LBC072");
+        require(_resignDelayBlocks >= 15, "LBC073");
         __Ownable_init_unchained();
         bridge = Bridge(_bridgeAddress);
         minCollateral = _minimumCollateral;
