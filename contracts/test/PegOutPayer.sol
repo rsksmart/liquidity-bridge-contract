2448b718f7a8af2e8c1af01918306e63d6eb2756
Luisfc68 <luisfc68@gmail.com>
2025-06-26 23:52:20 -0300

    fix: slither findings

--- contracts/test/PegOutPayer.sol
+++ contracts/test/PegOutPayer.sol
@@ -26,15 +26,15 @@ contract PegOutPayer {
     function executePegOut(QuotesV2.PegOutQuote memory quote, bytes memory signature) external {
         uint256 total = quote.value + quote.gasFee + quote.callFee + quote.productFeeAmount;
         require(total <= address(this).balance, "Insufficient balance in contract");
-        lbc.depositPegout{value: total}(quote, signature);
         emit PegOutPaid(quote, msg.sender);
+        lbc.depositPegout{value: total}(quote, signature);
     }
 
     function withdraw(uint256 amount) external {
         require(msg.sender == owner, "Only owner can withdraw");
         require(amount <= address(this).balance, "Insufficient balance in contract");
+        emit Withdraw(owner, amount);
         (bool sent, ) = payable(owner).call{value: amount}("");
         require(sent, "Failed to withdraw funds");
-        emit Withdraw(owner, amount);
     }
 }
