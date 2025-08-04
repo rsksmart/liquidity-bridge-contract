22d817ae8661d5324121cff96973482d59f76c5d
Luisfc68 <luisfc68@gmail.com>
2025-08-04 16:56:52 -0300

    docs: add natspec to dao contributor and pegout contract

--- contracts/DaoContributor.sol
+++ contracts/DaoContributor.sol
@@ -9,6 +9,11 @@ import {
 } from "@openzeppelin/contracts-upgradeable/utils/ReentrancyGuardUpgradeable.sol";
 import {Flyover} from "./libraries/Flyover.sol";
 
+/// @title OwnableDaoContributorUpgradeable
+/// @notice This contract is used to handle the contributions to the DAO
+/// @author Rootstock Labs
+/// @dev Any contract that inherits from this contract will be able to collect DAO
+/// contributions according to the logic the child contract defines
 abstract contract OwnableDaoContributorUpgradeable is
     ReentrancyGuardUpgradeable,
     OwnableUpgradeable {
@@ -24,13 +29,33 @@ abstract contract OwnableDaoContributorUpgradeable is
     bytes32 private constant _CONTRIBUTOR_STORAGE_LOCATION =
         0xb7e513d124139aa68259a99d4c2c344f3ba61e36716330d77f7fa887d0048e00;
 
+    /// @notice This event is emitted when a contribution is made to the DAO
+    /// @param contributor the address of the contributor
+    /// @param amount the amount of the contribution
     event DaoContribution(address indexed contributor, uint256 indexed amount);
+
+    /// @notice This event is emitted when the DAO fees are claimed. The claim is always all the
+    /// contributions made to the DAO by this contract so far
+    /// @param claimer the address of the claimer
+    /// @param receiver the address of the receiver
+    /// @param amount the amount of the fees claimed
     event DaoFeesClaimed(address indexed claimer, address indexed receiver, uint256 indexed amount);
+
+    /// @notice This event is emitted when the contributions are configured
+    /// @param feeCollector the address of the fee collector
+    /// @param feePercentage the percentage of the contributions that goes to the DAO
     event ContributionsConfigured(address indexed feeCollector, uint256 indexed feePercentage);
 
+    /// @notice This error is emitted when there are no fees to claim
     error NoFees();
+
+    /// @notice This error is emitted when the fee collector is not set
     error FeeCollectorUnset();
 
+    /// @notice This function is used to claim the contributions to the DAO.
+    /// It sends to the fee collector all the accumulated contributions so far
+    /// and resets the accumulated contributions to zero
+    /// @dev The function is only callable by the owner of the contract
     function claimContribution() external onlyOwner nonReentrant {
         DaoContributorStorage storage $ = _getContributorStorage();
         uint256 amount = $.currentContribution;
@@ -44,6 +69,10 @@ abstract contract OwnableDaoContributorUpgradeable is
         if (!sent) revert Flyover.PaymentFailed(feeCollector, amount, reason);
     }
 
+    /// @notice This function is used to configure the contributions to the DAO
+    /// @param feeCollector the address of the fee collector
+    /// @param feePercentage the percentage of the contributions that goes to the DAO
+    /// @dev The function is only callable by the owner of the contract
     function configureContributions(
         address payable feeCollector,
         uint256 feePercentage
@@ -54,18 +83,29 @@ abstract contract OwnableDaoContributorUpgradeable is
         emit ContributionsConfigured(feeCollector, feePercentage);
     }
 
+    /// @notice This function is used to get the fee percentage
+    /// that the child contracts use to calculate the contributions
+    /// @return feePercentage the fee percentage
     function getFeePercentage() external view returns (uint256) {
         return _getContributorStorage().feePercentage;
     }
 
+    /// @notice This function is used to get the current contribution
+    /// @return currentContribution the current contribution
     function getCurrentContribution() external view returns (uint256) {
         return _getContributorStorage().currentContribution;
     }
 
+    /// @notice This function is used to get the fee collector
+    /// @return feeCollector the fee collector address
     function getFeeCollector() external view returns (address) {
         return _getContributorStorage().feeCollector;
     }
 
+    /// @notice This function is used to initialize the contract
+    /// @param owner the owner of the contract
+    /// @param feePercentage the percentage that the child contracts use to calculate the contributions
+    /// @param feeCollector the address of the fee collector that will receive the contributions
     // solhint-disable-next-line func-name-mixedcase
     function __OwnableDaoContributor_init(
         address owner,
@@ -79,6 +119,11 @@ abstract contract OwnableDaoContributorUpgradeable is
         $.feeCollector = feeCollector;
     }
 
+    /// @notice This function is used to add a contribution to the DAO
+    /// @dev The function is only callable by the child contract, it can be
+    /// included wherever they consider the protocol should collect fees for the DAO
+    /// @param contributor the address of the contributor
+    /// @param amount the amount of the contribution
     function _addDaoContribution(address contributor, uint256 amount) internal {
         if (amount < 1) return;
         DaoContributorStorage storage $ = _getContributorStorage();
@@ -86,6 +131,9 @@ abstract contract OwnableDaoContributorUpgradeable is
         emit DaoContribution(contributor, amount);
     }
 
+    /// @dev The function is used to get the storage of the contract, avoid using the regular
+    /// storage to prevent conflicts with state variables of the child contract
+    /// @return $ the contributor storage
     function _getContributorStorage() private pure returns (DaoContributorStorage storage $) {
         assembly {
             $.slot := _CONTRIBUTOR_STORAGE_LOCATION
--- contracts/interfaces/PegOut.sol
+++ contracts/interfaces/PegOut.sol
@@ -3,18 +3,42 @@ pragma solidity 0.8.25;
 
 import {Quotes} from "../libraries/Quotes.sol";
 
+/// @title PegOut interface
+/// @notice This interface is used to expose the required functions to provide the Flyover peg out service
 interface IPegOut {
+
+    /// @notice Emitted when a peg out is refunded to the liquidity
+    /// provider after successfully providing the service
+    /// @param quoteHash hash of the refunded quote
     event PegOutRefunded(bytes32 indexed quoteHash);
+
+    /// @notice Emitted when a peg out is refunded to the user because
+    /// the liquidity provider failed to provide the service
+    /// @param quoteHash hash of the refunded quote
+    /// @param userAddress address of the refunded user
+    /// @param value refunded amount
     event PegOutUserRefunded(
         bytes32 indexed quoteHash,
         address indexed userAddress,
         uint256 indexed value
     );
+
+    /// @notice Emitted when a user overpays a peg out quote and the excess is bigger
+    /// than the dust threshold so its returned to the user
+    /// @param quoteHash hash of the paid quote
+    /// @param userAddress address of the user who is receiving the change
+    /// @param change the change amount
     event PegOutChangePaid(
         bytes32 indexed quoteHash,
         address indexed userAddress,
         uint256 indexed change
     );
+
+    /// Emitted when a peg out quote is paid
+    /// @param quoteHash hash of the paid quote
+    /// @param sender the payer of the quote
+    /// @param timestamp timestamp of the block where the quote was paid
+    /// @param amount the value of the deposit transaction
     event PegOutDeposit(
         bytes32 indexed quoteHash,
         address indexed sender,
@@ -22,32 +46,95 @@ interface IPegOut {
         uint256 amount
     );
 
+    /// @notice This error is emitted when the amount sent is less than the amount required to pay for the quote
+    /// @param amount the amount sent
+    /// @param target the amount required to pay for the quote
     error InsufficientAmount(uint256 amount, uint256 target);
+
+    /// @notice This error is emitted when the quote has expired by the number of blocks
+    /// @param expireBlock the number of blocks the quote has expired
     error QuoteExpiredByBlocks(uint32 expireBlock);
+
+    /// @notice This error is emitted when the quote has expired by the time
+    /// @param depositDateLimit the date limit for the user to deposit
+    /// @param expireDate the expiration of the quote
     error QuoteExpiredByTime(uint32 depositDateLimit, uint32 expireDate);
+
+    /// @notice This error is emitted when the quote has already been completed
+    /// @param quoteHash the hash of the quote that has already been completed
     error QuoteAlreadyCompleted(bytes32 quoteHash);
+
+    /// @notice This error is emitted when the quote has already been registered
+    /// @param quoteHash the hash of the quote that has already been registered
     error QuoteAlreadyRegistered(bytes32 quoteHash);
+
+    /// @notice This error is emitted when one of the output scripts of the Bitcoin
+    /// peg out transaction is malformed
+    /// @param outputScript the output script that is malformed
     error MalformedTransaction(bytes outputScript);
+
+    /// @notice This error is emitted when the destination of the Bitcoin transaction is invalid
+    /// @param expected the expected destination
+    /// @param actual the actual destination
     error InvalidDestination(bytes expected, bytes actual);
+
+    /// @notice This error is emitted when the quote hash is invalid
+    /// @param expected the expected quote hash
+    /// @param actual the actual quote hash
     error InvalidQuoteHash(bytes32 expected, bytes32 actual);
+
+    /// @notice This error is emitted when the sender is not allowed to perform a specific operation
+    /// @param expected the expected sender
+    /// @param actual the actual sender
     error InvalidSender(address expected, address actual);
+
+    /// @notice This error is emitted when the get confirmations from the rootstock bridge fails
+    /// @param errorCode The error code returned by the rootstock bridge
     error UnableToGetConfirmations(int errorCode);
+
+    /// @notice This error is emitted when the number of confirmations of the Bitcoin transaction is not enough
+    /// @param required the required number of confirmations
+    /// @param current the current number of confirmations
     error NotEnoughConfirmations(int required, int current);
+
+    /// @notice This error is emitted when the quote is not expired yet
+    /// @param quoteHash the hash of the quote that is not expired
     error QuoteNotExpired(bytes32 quoteHash);
 
+    /// @notice This is the function used to pay for a peg out quote. This is the only correct function to execute
+    /// such payment, sending money directly to the contract does not work
+    /// @param quote The quote that is being paid
+    /// @param signature The signature of the quote hash provided by the liquidity provider after the quote acceptance
     function depositPegOut(Quotes.PegOutQuote calldata quote, bytes calldata signature) external payable;
 
+    /// @notice This function is used by the liquidity provider to recover the funds spent on the peg out service plus
+    /// their fee for the service. It proves the inclusion of the transaction paying to the user in the Bitcoin network
+    /// @param quoteHash hash of the quote being refunded
+    /// @param btcTx the bitcoin raw transaction without the witness
+    /// @param btcBlockHeaderHash header hash of the block where the transaction was included
+    /// @param merkleBranchPath index of the leaf that is being proved to be included in the merkle tree
+    /// @param merkleBranchHashes hashes of the merkle branch to get to the merkle root using the leaf being proved
     function refundPegOut(
         bytes32 quoteHash,
         bytes calldata btcTx,
         bytes32 btcBlockHeaderHash,
-        uint256 partialMerkleTree,
+        uint256 merkleBranchPath,
         bytes32[] calldata merkleBranchHashes
     ) external;
 
+    /// @notice This function must be used by the user to recover the funds if the liquidity provider
+    /// fails to provide the service. The user needs to wait for the quote to expire before calling
+    /// this function
+    /// @param quoteHash the hash of the quote being refunded
     function refundUserPegOut(bytes32 quoteHash) external;
 
+    /// @notice This view is used to get the hash of a quote, this should be used as the single source of truth so
+    /// all the involved parties can compute the quote hash in the same way
+    /// @param quote the quote to hash
     function hashPegOutQuote(Quotes.PegOutQuote calldata quote) external view returns (bytes32);
 
+    /// @notice This view is used to check if a quote has been completed. Completed means it was paid and refunded
+    /// doesn't matter if the refund was to the liquidity provider (success) or to the user (failure)
+    /// @param quoteHash the hash of the quote to check
     function isQuoteCompleted(bytes32 quoteHash) external view returns (bool);
 }
