9e80db7f998b52cdef825f911b90e4a9f65e8ecf
Luisfc68 <luisfc68@gmail.com>
2025-08-04 16:56:52 -0300

    docs: add natspec to dao contributor and pegout contract

--- contracts/PegOutContract.sol
+++ contracts/PegOutContract.sol
@@ -10,15 +10,23 @@ import {Flyover} from "./libraries/Flyover.sol";
 import {Quotes} from "./libraries/Quotes.sol";
 import {SignatureValidator} from "./libraries/SignatureValidator.sol";
 
+/// @title PegOutContract
+/// @notice This contract is used to handle the peg out of the RSK network to the Bitcoin network
+/// @author Rootstock Labs
 contract PegOutContract is
     OwnableDaoContributorUpgradeable,
     IPegOut
 {
+    /// @notice This struct is used to store the information of a peg out
+    /// @param completed whether the peg out has been completed or not,
+    /// completed means the peg out was paid and refunded (to any party)
+    /// @param depositTimestamp the timestamp of the deposit
     struct PegOutRecord {
         bool completed;
         uint256 depositTimestamp;
     }
 
+    /// @notice The version of the contract
     string constant public VERSION = "1.0.0";
     Flyover.ProviderType constant private _PEG_TYPE = Flyover.ProviderType.PegOut;
     uint256 constant private _PAY_TO_ADDRESS_OUTPUT = 0;
@@ -32,13 +40,23 @@ contract PegOutContract is
     mapping(bytes32 => Quotes.PegOutQuote) private _pegOutQuotes;
     mapping(bytes32 => PegOutRecord) private _pegOutRegistry;
 
+    /// @notice The dust threshold for the peg out. If the difference between the amount paid and the amount required
+    /// is more than this value, the difference goes back to the user's wallet
     uint256 public dustThreshold;
+    /// @notice Average Bitcoin block time in seconds, used to calculate the expected confirmation time
     uint256 public btcBlockTime;
     bool private _mainnet;
 
+    /// @notice This event is emitted when the dust threshold is set
+    /// @param oldThreshold the old dust threshold
+    /// @param newThreshold the new dust threshold
     event DustThresholdSet(uint256 indexed oldThreshold, uint256 indexed newThreshold);
+    /// @notice This event is emitted when the Bitcoin block time is set
+    /// @param oldTime the old Bitcoin block time
+    /// @param newTime the new Bitcoin block time
     event BtcBlockTimeSet(uint256 indexed oldTime, uint256 indexed newTime);
 
+    /// @inheritdoc IPegOut
     function depositPegOut(
         Quotes.PegOutQuote calldata quote,
         bytes calldata signature
@@ -88,6 +106,16 @@ contract PegOutContract is
         }
     }
 
+    /// @notice This function is used to initialize the contract
+    /// @param owner the owner of the contract
+    /// @param bridge the address of the Rootstock bridge
+    /// @param dustThreshold_ the dust threshold for the peg out
+    /// @param collateralManagement the address of the Collateral Management contract
+    /// @param mainnet whether the contract is on the mainnet or not
+    /// @param btcBlockTime_ the average Bitcoin block time in seconds
+    /// @param daoFeePercentage the percentage of the peg out amount that goes to the DAO.
+    /// Use zero to disable the DAO integration feature
+    /// @param daoFeeCollector the address of the DAO fee collector
     // solhint-disable-next-line comprehensive-interface
     function initialize(
         address owner,
@@ -108,6 +136,9 @@ contract PegOutContract is
         btcBlockTime = btcBlockTime_;
     }
 
+    /// @notice This function is used to set the collateral management contract
+    /// @param collateralManagement the address of the Collateral Management contract
+    /// @dev This function is only callable by the owner of the contract
     // solhint-disable-next-line comprehensive-interface
     function setCollateralManagement(address collateralManagement) external onlyOwner {
         if (collateralManagement.code.length == 0) revert Flyover.NoContract(collateralManagement);
@@ -115,23 +146,30 @@ contract PegOutContract is
         _collateralManagement = ICollateralManagement(collateralManagement);
     }
 
+    /// @notice This function is used to set the dust threshold
+    /// @param threshold the new dust threshold
+    /// @dev This function is only callable by the owner of the contract
     // solhint-disable-next-line comprehensive-interface
     function setDustThreshold(uint256 threshold) external onlyOwner {
         emit DustThresholdSet(dustThreshold, threshold);
         dustThreshold = threshold;
     }
 
+    /// @notice This function is used to set the average Bitcoin block time
+    /// @param blockTime the new average Bitcoin block time in seconds
+    /// @dev This function is only callable by the owner of the contract
     // solhint-disable-next-line comprehensive-interface
     function setBtcBlockTime(uint256 blockTime) external onlyOwner {
         emit BtcBlockTimeSet(btcBlockTime, blockTime);
         btcBlockTime = blockTime;
     }
 
+    /// @inheritdoc IPegOut
     function refundPegOut(
         bytes32 quoteHash,
         bytes calldata btcTx,
         bytes32 btcBlockHeaderHash,
-        uint256 partialMerkleTree,
+        uint256 merkleBranchPath,
         bytes32[] calldata merkleBranchHashes
     ) external nonReentrant override {
         if(!_collateralManagement.isRegistered(_PEG_TYPE, msg.sender)) {
@@ -145,7 +183,7 @@ contract PegOutContract is
 
         BtcUtils.TxRawOutput[] memory outputs = BtcUtils.getOutputs(btcTx);
         _validateBtcTxNullData(outputs, quoteHash);
-        _validateBtcTxConfirmations(quote, btcTx, btcBlockHeaderHash, partialMerkleTree, merkleBranchHashes);
+        _validateBtcTxConfirmations(quote, btcTx, btcBlockHeaderHash, merkleBranchPath, merkleBranchHashes);
         _validateBtcTxAmount(outputs, quote);
         _validateBtcTxDestination(outputs, quote);
 
@@ -166,6 +204,7 @@ contract PegOutContract is
         }
     }
 
+    /// @inheritdoc IPegOut
     function refundUserPegOut(bytes32 quoteHash) external nonReentrant override {
         Quotes.PegOutQuote memory quote = _pegOutQuotes[quoteHash];
 
@@ -188,16 +227,22 @@ contract PegOutContract is
         }
     }
 
+    /// @inheritdoc IPegOut
     function hashPegOutQuote(
         Quotes.PegOutQuote calldata quote
     ) external view override returns (bytes32) {
         return _hashPegOutQuote(quote);
     }
 
+    /// @inheritdoc IPegOut
     function isQuoteCompleted(bytes32 quoteHash) external view override returns (bool) {
         return _isQuoteCompleted(quoteHash);
     }
 
+    /// @notice This function is used to hash a peg out quote
+    /// @dev The function also validates the quote belongs to this contract
+    /// @param quote the peg out quote to hash
+    /// @return quoteHash the hash of the peg out quote
     function _hashPegOutQuote(
         Quotes.PegOutQuote calldata quote
     ) private view returns (bytes32) {
@@ -207,10 +252,21 @@ contract PegOutContract is
         return keccak256(Quotes.encodePegOutQuote(quote));
     }
 
+    /// @notice This function is used to check if a quote has been completed (refunded by any party)
+    /// @param quoteHash the hash of the quote to check
+    /// @return completed whether the quote has been completed or not
     function _isQuoteCompleted(bytes32 quoteHash) private view returns (bool) {
         return _pegOutRegistry[quoteHash].completed;
     }
 
+    /// @notice This function is used to check if a liquidity provider should be penalized
+    /// according to the following rules:
+    /// - If the transfer was not made on time, the liquidity provider should be penalized
+    /// - If the liquidity provider is refunding after expiration, the liquidity provider should be penalized
+    /// @param quote the peg out quote
+    /// @param quoteHash the hash of the quote
+    /// @param blockHash the hash of the block that contains the first confirmation of the peg out transaction
+    /// @return shouldPenalize whether the liquidity provider should be penalized or not
     function _shouldPenalize(
         Quotes.PegOutQuote memory quote,
         bytes32 quoteHash,
@@ -237,17 +293,24 @@ contract PegOutContract is
         return false;
     }
 
+    /// @notice This function is used to validate the number of confirmations of the Bitcoin transaction.
+    /// The function interacts with the Rootstock bridge to get the number of confirmations
+    /// @param quote the peg out quote
+    /// @param btcTx the Bitcoin transaction
+    /// @param btcBlockHeaderHash the hash of the block that contains the first confirmation of the peg out transaction
+    /// @param merkleBranchPath the path of the merkle branch
+    /// @param merkleBranchHashes the hashes of the merkle branch
     function _validateBtcTxConfirmations(
         Quotes.PegOutQuote memory quote,
         bytes calldata btcTx,
         bytes32 btcBlockHeaderHash,
-        uint256 partialMerkleTree,
+        uint256 merkleBranchPath,
         bytes32[] calldata merkleBranchHashes
     ) private view {
         int256 confirmations = _bridge.getBtcTransactionConfirmations(
             BtcUtils.hashBtcTx(btcTx),
             btcBlockHeaderHash,
-            partialMerkleTree,
+            merkleBranchPath,
             merkleBranchHashes
         );
         if (confirmations < 0) {
@@ -257,6 +320,9 @@ contract PegOutContract is
         }
     }
 
+    /// @notice This function is used to validate the destination of the Bitcoin transaction
+    /// @param outputs the outputs of the Bitcoin transaction
+    /// @param quote the peg out quote
     function _validateBtcTxDestination(
         BtcUtils.TxRawOutput[] memory outputs,
         Quotes.PegOutQuote memory quote
@@ -270,6 +336,9 @@ contract PegOutContract is
         }
     }
 
+    /// @notice This function is used to validate the amount of the Bitcoin transaction
+    /// @param outputs the outputs of the Bitcoin transaction
+    /// @param quote the peg out quote
     function _validateBtcTxAmount(
         BtcUtils.TxRawOutput[] memory outputs,
         Quotes.PegOutQuote memory quote
@@ -282,6 +351,10 @@ contract PegOutContract is
         if (paidAmount < requiredAmount) revert InsufficientAmount(requiredAmount, paidAmount);
     }
 
+    /// @notice This function is used to validate the null data of the Bitcoin transaction. The null data
+    /// is used to store the hash of the peg out quote in the Bitcoin transaction
+    /// @param outputs the outputs of the Bitcoin transaction
+    /// @param quoteHash the hash of the peg out quote
     function _validateBtcTxNullData(BtcUtils.TxRawOutput[] memory outputs, bytes32 quoteHash) private pure {
         bytes memory scriptContent = BtcUtils.parseNullDataScript(outputs[_QUOTE_HASH_OUTPUT].pkScript);
         uint256 scriptLength = scriptContent.length;
