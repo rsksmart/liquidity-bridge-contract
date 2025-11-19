9dcb263d59082d0b0e68545b2d5adbb3633b7e8f
Hakob23 <hakob.arshakyan23@gmail.com>
2025-11-19 22:37:22 +0400

    Refactor EmergencyPause contracts by removing EmergencyPauser and EmergencyPauserPeg

--- src/EmergencyPause/EmergencyPauserDefaultAdmin.sol
+++ src/EmergencyPause/EmergencyPauserDefaultAdmin.sol
@@ -6,9 +6,35 @@ pragma solidity 0.8.25;
 import {
     AccessControlDefaultAdminRulesUpgradeable
 } from "@openzeppelin/contracts-upgradeable/access/extensions/AccessControlDefaultAdminRulesUpgradeable.sol";
-import {EmergencyPauser} from "./EmergencyPauser.sol";
+import {PausableUpgradeable} from "@openzeppelin/contracts-upgradeable/utils/PausableUpgradeable.sol";
 
-abstract contract EmergencyPauserDefaultAdmin is EmergencyPauser, AccessControlDefaultAdminRulesUpgradeable {
+abstract contract EmergencyPauserDefaultAdmin is AccessControlDefaultAdminRulesUpgradeable, PausableUpgradeable {
+
+    bytes32 internal constant _PAUSER_ROLE = keccak256("PAUSER_ROLE");
+
+    string private _pauseReason;
+    uint64 private _pauseTimestamp;
+
+    event EmergencyPaused(address indexed by, string reason);
+    event EmergencyUnpaused(address indexed by);
+
+    function pauseStatus() external view returns (bool isPaused, string memory reason, uint64 since) {
+        return (paused(), _pauseReason, _pauseTimestamp);
+    }
+
+    function _emergencyPause(string calldata reason) internal {
+        _pause();
+        _pauseReason = reason; // storage string or reason hash/URI
+        _pauseTimestamp = uint64(block.timestamp);
+        emit EmergencyPaused(msg.sender, reason);
+    }
+
+    function _emergencyUnpause() internal {
+        _pauseReason = "";
+        _pauseTimestamp = 0;
+        _unpause();
+        emit EmergencyUnpaused(msg.sender);
+    }
 
     /// @notice Pauses the contract
     /// @param reason The reason for pausing
