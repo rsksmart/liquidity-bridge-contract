77cb8c6805a90a55147a24ca1a9fa8b23b4a3981
Hakob23 <hakob.arshakyan23@gmail.com>
2025-12-11 00:34:41 +0400

    refactor: deployment logs

--- script/deployment/DeployPegOut.s.sol
+++ script/deployment/DeployPegOut.s.sol
@@ -6,7 +6,6 @@ import {Script, console} from "lib/forge-std/src/Script.sol";
 import {HelperConfig} from "../HelperConfig.s.sol";
 
 import {PegOutContract} from "../../src/PegOutContract.sol";
-import {CollateralManagementContract} from "../../src/CollateralManagement.sol";
 import {TransparentUpgradeableProxy} from "@openzeppelin/contracts/proxy/transparent/TransparentUpgradeableProxy.sol";
 import {ProxyAdmin} from "@openzeppelin/contracts/proxy/transparent/ProxyAdmin.sol";
 
@@ -92,11 +91,7 @@ contract DeployPegOut is Script {
 
         // Sanity check
         PegOutContract pegOut = PegOutContract(payable(address(proxy)));
-        console.log(
-            "Version:",
-            CollateralManagementContract(payable(collateralManagementProxy))
-                .VERSION()
-        );
+        console.log("PegOutContract version:", pegOut.VERSION());
         console.log("PegOutContract btcBlockTime:", pegOut.btcBlockTime());
 
         result = DeploymentResult({
