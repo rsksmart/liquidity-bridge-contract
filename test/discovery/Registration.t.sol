c2cdea7072987878613e308967209de2ff15675d
Hakob23 <hakob.arshakyan230@gmail.com>
2026-02-20 00:56:37 +0400

    fmt: fix

--- test/discovery/Registration.t.sol
+++ test/discovery/Registration.t.sol
@@ -191,9 +191,7 @@ contract RegistrationTest is DiscoveryTestBase {
         );
     }
 
-    function test_Register_PreventsMultipleRegistrationsBySameEOA()
-        public
-    {
+    function test_Register_PreventsMultipleRegistrationsBySameEOA() public {
         address lp = makeAddr("lp");
         vm.deal(lp, 100 ether);
 
