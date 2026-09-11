035302e68ac1be6ecde7dc9cab56852179a429af
Hakob23 <hakob.arshakyan23@gmail.com>
2026-09-11 16:00:22 +0400

    feat: enhance PegOutContract with deposit validation and change handling

--- src/PegOutEscrow.sol
+++ src/PegOutEscrow.sol
@@ -161,6 +161,7 @@ contract PegOutEscrow is
             nonce,
             confirmations
         );
+        emit PegOutRequested(requestHash, refundAddress, amount, destinationAddress);
 
         if (changeRefund > 0) {
             emit EscrowPegOutChangePaid(requestHash, refundAddress, changeRefund);
@@ -365,8 +366,6 @@ contract PegOutEscrow is
         $.state[requestHash] = EscrowedPegOutState.REQUESTED;
         $.requestHashByNonce[nonce] = requestHash;
         $.quotes[requestHash] = quote;
-
-        emit PegOutRequested(requestHash, refundAddress, amount, destinationAddress);
     }
 
     function _terminate(
